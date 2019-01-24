443f4f026541476860cb521c58e021a17580bdac
cloudDD <351426081@qq.com>
2019-01-24 17:17:32 +0800

    mp

--- 37.py
+++ 37.py
@@ -1,4 +1,19 @@
 # 对10个数进行排序。
 
+#冒泡
 
-def orderList():
+
+def orderlist():
+
+    a = [4,1,4,40,31,45,23]
+
+    for i in range(len(a)):
+        for j in range(len(a)-1):
+            if a[j] < a[j+1]:
+                a[j],a[j+1] = a[j+1],a[j]
+
+    print(a)
+
+
+if __name__ == "__main__":
+    orderlist()
--- practice.py
+++ practice.py
@@ -1,14 +1,4 @@
-a = int(input("请输入一个数字:\n"))
-x = str(a)
-flag = True
+a ="wfew fwe sef@@#"
 
-for i in range(len(x) / 2):
-    if x[i] != x[-i - 1]:
-        flag = False
-        break
-if flag:
-    print
-    "%d 是一个回文数!" % a
-else:
-    print
-    "%d 不是一个回文数!" % a
+for c in a :
+    print(c)
